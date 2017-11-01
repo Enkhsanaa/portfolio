52f7bfd0b8899b30c02bd02a5947b0a91a4d626a
f1kir <sanaenkh@gmail.com>
2017-11-01 16:15:53 +0800

    POST TTS test 2

--- src/app/mn-speech-api/mn-speech-api.component.ts
+++ src/app/mn-speech-api/mn-speech-api.component.ts
@@ -8,30 +8,41 @@ import { Http, Headers } from '@angular/http';
 })
 export class MnSpeechApiComponent implements OnInit {
 	message: String;
+	mToken: String;
 	constructor(private http: Http) {}
 
 	ngOnInit() {}
 	sendToTTS(token, text) {
-		var request = {
+		let request = {
 			url: 'tts/',
 			token: token,
-			data: { txt: text }
+			data: 'txt=' + text
 		};
-		var headers = new Headers();
+		let headers = new Headers();
 		headers.append('Authorization', 'JWT ' + request.token);
 		headers.append('Content-Type', 'application/x-www-form-urlencoded');
-		var reader = new FileReader();
+		let reader = new FileReader();
 		reader.onload = function(e) {
 			window.open(decodeURIComponent(reader.result), '_self', '', false);
 		};
 		this.http.post(request.url, request.data, { headers: headers }).subscribe(res => {
-			console.log(res);
-			if (res.status == 200) {
-				reader.readAsDataURL(new Blob([res], { type: 'audio/wav' }));
-			} else this.message = res.statusText;
+			reader.readAsDataURL(new Blob([res], { type: 'audio/wav' }));
 		});
 	}
-	sendToSTT() {
-		return '';
+	sendToSTT() {}
+	getToken(fname, lname, phone, email, username, password, aboutProject) {
+		let data = 'fname=' + fname + 'lname=' + lname;
+		data += 'phone=' + phone + 'email=' + email;
+		data += 'username=' + username + 'password=' + password;
+		data += 'aboutProject=' + aboutProject;
+
+		var headers = new Headers();
+		headers.append('Content-Type', 'application/x-www-form-urlencoded');
+		this.http.post('user', data, { headers: headers }).subscribe(res => {
+			var resp = JSON.parse(res.text());
+			if (!resp.success) {
+				this.mToken = resp.message;
+			} else this.mToken = resp.token;
+		});
 	}
 }
