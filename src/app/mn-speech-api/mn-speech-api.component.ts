2560182b4e055ebc2e21ad5e7c352561fb3c59fc
f1kir <sanaenkh@gmail.com>
2017-11-02 11:06:43 +0800

    POST TTS test 3

--- src/app/mn-speech-api/mn-speech-api.component.ts
+++ src/app/mn-speech-api/mn-speech-api.component.ts
@@ -1,48 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-
+import 'rxjs/Rx';
 @Component({
 	selector: 'app-mn-speech-api',
 	templateUrl: './mn-speech-api.component.html',
 	styleUrls: ['./mn-speech-api.component.scss']
 })
 export class MnSpeechApiComponent implements OnInit {
-	message: String;
-	mToken: String;
+	flashMessage: { content: String, type: String};
+	responseWAV: String = '';
+	authToken: String = '';
 	constructor(private http: Http) {}
 
-	ngOnInit() {}
-	sendToTTS(token, text) {
+	ngOnInit() {
+		this.flashMessage = {
+			content: '',
+			type: ''
+		};
+		this.loadToken();
+	}
+	sendToTTS(token, voice, text) {
 		let request = {
-			url: 'tts/',
-			token: token,
-			data: 'txt=' + text
+			url: 'https://enkhsanaa.me/tts/',
+			token: token.value,
+			data: { txt: text.value, voice: voice.value}
 		};
 		let headers = new Headers();
 		headers.append('Authorization', 'JWT ' + request.token);
-		headers.append('Content-Type', 'application/x-www-form-urlencoded');
+		headers.append('Content-Type', 'application/json');
 		let reader = new FileReader();
 		reader.onload = function(e) {
 			window.open(decodeURIComponent(reader.result), '_self', '', false);
 		};
-		this.http.post(request.url, request.data, { headers: headers }).subscribe(res => {
-			reader.readAsDataURL(new Blob([res], { type: 'audio/wav' }));
+		this.http.post(request.url, request.data, { headers: headers }).map((data: any) => data.json())
+			.subscribe((data: any) => {
+			if (!data.success) {
+				this.responseWAV = 'Алдаа!';
+				return;
+			}
+			else this.responseWAV = data.fileName;
 		});
 	}
 	sendToSTT() {}
 	getToken(fname, lname, phone, email, username, password, aboutProject) {
-		let data = 'fname=' + fname + 'lname=' + lname;
-		data += 'phone=' + phone + 'email=' + email;
-		data += 'username=' + username + 'password=' + password;
-		data += 'aboutProject=' + aboutProject;
+		let data = {
+			fname: fname.value,
+			lname: lname.value,
+			phone: phone.value,
+			email: email.value,
+			username: username.value,
+			password: password.value,
+			aboutProject: aboutProject.value
+		};
 
 		var headers = new Headers();
-		headers.append('Content-Type', 'application/x-www-form-urlencoded');
-		this.http.post('user', data, { headers: headers }).subscribe(res => {
-			var resp = JSON.parse(res.text());
-			if (!resp.success) {
-				this.mToken = resp.message;
-			} else this.mToken = resp.token;
-		});
+		headers.append('Content-Type', 'application/json');
+		this.http
+			.post('https://enkhsanaa.me/user', data, { headers: headers })
+			.map((data: any) => data.json())
+			.subscribe((data: any) => {
+				console.log(data);
+				if (!data.success) {
+					this.flashMessage.content = data.message;
+					this.flashMessage.type = 'danger';
+					setTimeout(() => {
+						this.flashMessage.content = '';
+					}, 2000);
+					return;
+				}
+				this.flashMessage.content = 'Бүртгэл амжилттай! Таны токен ' + data.token;
+				this.flashMessage.type = 'success';
+				this.storeUserData(data.token);
+			});
+	}
+	loadToken() {
+		const token = localStorage.getItem('mnSpeechAPIToken') || '';
+		this.authToken = token;
+	}
+	storeUserData(token) {
+		localStorage.setItem('mnSpeechAPIToken', token);
+		this.authToken = token;
 	}
 }
